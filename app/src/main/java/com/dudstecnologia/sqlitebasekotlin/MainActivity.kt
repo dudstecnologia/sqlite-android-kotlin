c61fa6278ae2b09b7a4299306219fbd7ec645e4b
Eduardo Silva <eduardo@dudstecnologia.com.br>
2022-08-31 06:24:26 -0400

    Aula 2

--- app/src/main/java/com/dudstecnologia/sqlitebasekotlin/MainActivity.kt
+++ app/src/main/java/com/dudstecnologia/sqlitebasekotlin/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.dudstecnologia.sqlitebasekotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dudstecnologia.sqlitebasekotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bindig: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bindig = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindig.root)
+
+        bindig.btnOpenRegister.setOnClickListener {
+            val intentForm = Intent(this, RegisterActivity::class.java)
+            startActivity(intentForm)
+        }
     }
 }
